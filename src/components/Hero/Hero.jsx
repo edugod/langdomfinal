cd8d27627eee7bc227750edce43e6c817d500200
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2024-01-10 21:06:32 -0300

    ffs

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,84 @@
 import React from 'react'
 import './Hero.css'
+import { HiLocationMarker } from 'react-icons/hi'
+import CountUp from 'react-countup'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
 	return (
 		<section className='hero-wrapper'>
-			<div className='paddings innerWidth flexCenter hero-container'>
-				{/* LADO ESQUERDO */}
+			<div className='paddings innerWidth flexCenter  hero-container '>
+				{/* LEFT SIDE */}
 				<div className='flexColStart hero-left'>
 					<div className='hero-title'>
-						<div className='orange-circle'>
-							<h1>
-								Langdom <br /> Idiomas <br /> Lorem, ipsum dolor.
-							</h1>
-						</div>
+						<div className='orange-circle' />
+						<motion.h1
+							initial={{ y: '2rem', opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{
+								duration: '2',
+								type: 'spring',
+							}}
+						>
+							Langdom <br />
+							Most Suitable <br />
+							Property
+						</motion.h1>
+					</div>
 
-						<div className='flexColStart hero-des'>
-							<span className='secondaryText'>
-								{' '}
-								Find a veriety of properties that suit you very easily
+					<div className='flexColStart hero-des'>
+						<span className='secondaryText'>
+							Find a veriety of properties that suit you very easily
+						</span>
+						<span className='secondaryText'>
+							Forget all difficulties in finding a residence for you
+						</span>
+					</div>
+
+					<div className='flexCenter search-bar'>
+						<HiLocationMarker color='var(--blue)' size={25} />
+						<input type='text' />
+						<button className='button'>Search</button>
+					</div>
+
+					<div className='flexCenter stats'>
+						<div className='flexColCenter stat'>
+							<span>
+								<CountUp start={8800} end={9000} duration={4} />
+								<span>+</span>
 							</span>
-							<span className='secondaryText'>
-								{' '}
-								Forget all difficulties in finding a residence for you
+							<span className='secondaryText'>Premium Products</span>
+						</div>
+						<div className='flexColCenter stat'>
+							<span>
+								<CountUp start={1950} end={2000} duration={4} />
+								<span>+</span>
 							</span>
+							<span className='secondaryText'>Happy Custormers</span>
+						</div>
+						<div className='flexColCenter stat'>
+							<span>
+								<CountUp end={28} />
+								<span>+</span>
+							</span>
+							<span className='secondaryText'>Award Winning</span>
 						</div>
 					</div>
 				</div>
-        {/* LADO DIREITO */}
-
-        <div className="flexCenter hero-right">
-          <div className="image-container">
-            <img src="./escolita.JPG" alt="imagem da escola" />
-          </div>
-        </div>
+				{/* RIGHT SIDE */}
+				<div className='flexCenter hero-right'>
+					<motion.div
+						initial={{ x: '10rem', opacity: '0' }}
+						animate={{ x: '0', opacity: '1' }}
+						transition={{
+							duration: 2,
+							type: 'spring',
+						}}
+						className='image-container'
+					>
+						<img src='./escolita.JPG' alt='langdom' />
+					</motion.div>
+				</div>
 			</div>
 		</section>
 	)
